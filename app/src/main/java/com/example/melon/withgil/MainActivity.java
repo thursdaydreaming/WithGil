b41c1fb501bdf78455a9babb6699f7410eb637cf
melongl11 <melongl11@naver.com>
2018-08-22 23:25:24 +0900

    구글맵 추가, Floating Menu 및 Button 설정 중....

--- app/src/main/java/com/example/melon/withgil/MainActivity.java
+++ app/src/main/java/com/example/melon/withgil/MainActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
